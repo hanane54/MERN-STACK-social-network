1c772565688d5ccd604db96e29f4d57913b674ae
hanane54 <hanane59taouil@gmail.com>
2022-06-22 16:01:22 +0100

    Added: the profile routes, Adding: posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,108 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // Private routes are not accessible by non athenticated users
 // you need to have a token to havee access to them unlike Public routes
 
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required").not().isEmpty(),
+      check("question", "Question is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        question: req.body.question,
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
 // @route    GET api/posts
-// @desc     test route
-// @access   Public
-router.get('/', (req, res) => res.send('Posts route'));
+// @desc     Get all posts
+// @access   Private
+router.get('/', auth, async (req, res)=> {
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    }
+});
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', auth, async (req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({msg: "Post not found"});
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(!err.kind=== 'ObjectId'){
+            return res.status(404).json({msg: "Post not found"});
+        }
+      res.status(500).send("Internal Server Error");
+    }
+});
+
+// @route    DELETE api/posts
+// @desc     Delete a post
+// @access   Private
+router.delete('/:id', auth, async (req, res)=> {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Verify that the user who is deleting is the post owner
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({msg: "Post removed"});
+    } catch (err) {
+        console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = router;
