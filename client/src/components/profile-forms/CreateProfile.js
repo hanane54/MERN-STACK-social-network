64a0bbae7f94e14d3618aaba2623cf2453bac7ef
hanane54 <hanane59taouil@gmail.com>
2022-06-26 11:50:57 +0100

    added: profile redux Action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
 
-const CreateProfile = () => {
+const CreateProfile = ({ createProfile}) => {
   const [formData, setFormData] = useState({
     company: "",
     website: "",
@@ -17,6 +19,8 @@ const CreateProfile = () => {
     youtube: "",
     instagram: "",
   });
+
+  const navigate = useNavigate();
 
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
@@ -38,6 +42,11 @@ const CreateProfile = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, navigate);
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -46,7 +55,7 @@ const CreateProfile = () => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)} >
         <div className="form-group">
           <select name="status" value={status} onChange={(e) => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -213,6 +222,8 @@ const CreateProfile = () => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default connect()(CreateProfile);
+export default connect(null, { createProfile })(CreateProfile);
